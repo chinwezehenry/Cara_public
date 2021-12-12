4e357e69578303c116c9469b5d89f02bf5917498
chinwezehenry <89517371+chinwezehenry@users.noreply.github.com>
2021-12-12 04:24:23 +0100

    Update Hi.ts

--- src/commands/General/Hi.ts
+++ src/commands/General/Hi.ts
@@ -27,7 +27,7 @@ export default class Command extends BaseCommand {
 			{
 				quoted: M.WAMessage,
 				mimetype: Mimetype.gif,
-				caption: `I don't have time to have a conversation with someone like you. Use something from *${this.client.config.prefix}help* list if you want anything. \n`,
+				caption: `Hello,this is cara bot. To find my command menu,type *${this.client.config.prefix}help*. \n`,
 			}
 		);
 	};
